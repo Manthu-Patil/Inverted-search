2e864575b1cb987f88ce93f369188651d6c2ba46
Mahantesh Patil <manthupatil74@gmail.com>
2023-12-28 18:09:17 +0530

    link == NULL successfull

--- func.c
+++ func.c
@@ -2,10 +2,13 @@
 #include "inverted_search.h"
 #include<string.h>
 #include<stdlib.h>
+sll *head=NULL;
+int flag=0;
 Status validation(int argc,char *argv[])
 {
     int i;
-    sll *head=NULL;
+    word_node w_node;
+    sub_node s_node;
     if(argc>1)
     {
         for(i=1;i<argc;i++)
@@ -57,18 +60,28 @@ Status check_file_empty(char *fname)
     FILE *fptr=fopen(fname,"r");
     fseek(fptr,0,SEEK_END);
     if(ftell(fptr)>0)
+    {
+        fclose(fptr);
         return success;
+    }
     else
+    {
         return failure;
+    }
 }
 
 Status check_file_exists(char *fname)
 {
     FILE *fptr=fopen(fname,"r");
     if(fptr)
+    {
+        fclose(fptr);
         return success;
+    }
     else
+    {
         return failure;
+    }
 }
 
 Status insert(sll **head,char *fname)
@@ -78,31 +91,31 @@ Status insert(sll **head,char *fname)
         return failure;
     else
     {
-      strcpy(New->file_name,fname);
-      New->link=NULL;
+        strcpy(New->file_name,fname);
+        New->link=NULL;
 
-      if(*head==NULL)
-      {
-          *head=New;
-          (*head)->link=NULL;
-          return success;
-      }
+        if(*head==NULL)
+        {
+            *head=New;
+            (*head)->link=NULL;
+            return success;
+        }
 
-     sll *temp=*head;
-     sll *prev=*head;
+        sll *temp=*head;
+        sll *prev=*head;
 
-      while(temp)
-      {
-         if((strcmp(temp->file_name,fname)==0))
-         {
-             printf("%s is duplicate file\n",fname);
-            return failure;
-         }
-         prev=temp;
-         temp=temp->link;
-      }
-      prev->link=New;
-      return success;
+        while(temp)
+        {
+            if((strcmp(temp->file_name,fname)==0))
+            {
+                printf("%s is duplicate file\n",fname);
+                return failure;
+            }
+            prev=temp;
+            temp=temp->link;
+        }
+        prev->link=New;
+        return success;
     }
 }
 
@@ -116,4 +129,115 @@ void print_list(sll *head)
         temp=temp->link;
     }
     printf("<-NULL\n");
+}
+
+Status create_database(sll **head,m_table *m_table)
+{
+    sll *temp=*head;
+    char str[50];
+    int index;
+    int break_flag=0;
+    while(temp)
+    {
+        FILE *f_ptr=fopen(temp->file_name,"r");
+        while(!feof(f_ptr))
+        {
+            fscanf(f_ptr,"%s",str);
+            index=str[0] % 97;
+            word_node *new_wnode=malloc(sizeof(word_node));
+            sub_node *new_snode=malloc(sizeof(sub_node));
+            //create subnode
+            new_snode->word_count=1;
+            strcpy(new_snode->file_name,temp->file_name);
+            new_snode->subnode_link=NULL;
+            //create word node
+            new_wnode->file_count=1;
+            strcpy(new_wnode->word,str);
+            new_wnode->subnode_link=new_snode;
+            new_wnode->wordnode_link=NULL;
+
+            //Index link is Null
+            if(m_table[index].link==NULL)
+            {
+                m_table[index].link=new_wnode;
+            }
+            /*
+            //Index link is not Null
+            else
+            {
+                word_node *w_temp=m_table[index].link;
+                word_node *prev_wtemp=NULL;
+                sub_node *s_temp=w_temp->subnode_link;
+                sub_node *prev_stemp=NULL;
+                while(w_temp)
+                {
+                    //word matches
+                    if(!strcmp(w_temp->word,new_wnode->word))
+                    {
+                        while(s_temp)
+                        {
+
+                            //word and file matches
+                            if(!strcmp(s_temp->file_name,new_snode->file_name))
+                            {
+                                (s_temp->word_count)++;
+                                //break_flag=1;
+                                // break;
+                            }
+
+                            //word matches file doesn't
+                            prev_stemp=s_temp;
+                            s_temp=s_temp->subnode_link;
+                        }
+                        prev_stemp->subnode_link=new_snode;
+                        //(w_temp->file_count)++;
+
+                    }
+                    //word doesnt match traverse
+                    prev_wtemp=w_temp;
+                    w_temp=w_temp->wordnode_link;
+                }
+                prev_wtemp->wordnode_link=new_wnode;               
+            }
+            */
+
+
+        }
+        temp=temp->link;
+    }
+    flag=1;
+    return success;
+}
+
+Status display_database(m_table m_table[])
+{
+    if(flag==1)
+    {
+        for(int i=0;i<index_size;i++)
+        {
+            if(m_table[i].link!=NULL)
+            {
+                word_node *w_temp=m_table[i].link;
+                sub_node *s_temp=w_temp->subnode_link;
+                while(w_temp!=NULL)
+                {
+                    printf("Word: [%s] ",w_temp->word);
+                    printf("Filecount: %d ",w_temp->file_count);
+                    while(s_temp!=NULL)
+                    {
+                        printf("Wordcount: %d ",s_temp->word_count);
+                        printf("Files: %s \n",s_temp->file_name);
+                        s_temp=s_temp->subnode_link;
+                    }
+                    w_temp=w_temp->wordnode_link;
+                }
+            }
+        }
+        return success;
+    }
+    else
+    {
+        printf("Database is not created. No data to display\n");
+        return failure;
+    }
 }
--- main.c
+++ main.c
@@ -1,15 +1,62 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "inverted_search.h"
+extern sll *head;
 int main(int argc,char **argv)
 {
+    int choice;
+    m_table main_table[index_size];
+
+    for(int i=0;i<index_size;i++)
+    {
+        main_table[i].index=i;
+        main_table[i].link=NULL;
+    }
+
     if(validation(argc,argv)==success)
     {
         printf("Validation successfull\n");
+        printf("-----------------------------------------INVERTED SEARCH----------------------------------------\n");
+        while(1)
+        {
+            printf("1-Create Database\n2-Display Database\n3-Exit\n");
+            printf("Enter your choice: ");
+
+            scanf("%d",&choice);
+            switch(choice)
+            {
+                case 1: 
+                    if(create_database(&head,main_table)==success)
+                    {
+                        printf("Create database successfull\n");
+                    }
+                    else
+                    {
+                        printf("Create database failure\n");
+                    }
+                    break;
+                case 2:
+                    if(display_database(main_table)==success)
+                    {
+                        printf("Display database successfull\n");
+                    }
+                    else
+                    {
+                        printf("Display database failure\n");
+                    }
+                    break;
+                case 3:
+                    exit(0);
+                default: printf("Invalid choice\n");
+
+            }
+        }
     }
     else
     {
         printf("Validation failed\n");
     }
+
     return 0;
 }
 
